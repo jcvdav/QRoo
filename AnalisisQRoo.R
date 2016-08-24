03f3a46f2404b5dbf9d63bf20e042dd2807736ce
jcvdav <juanarlos.villader@gmail.com>
2016-08-24 12:07:36 -0700

    RScritpt para análisis de Quintana Roo

---
+++ AnalisisQRoo.R
@@ -0,0 +1,63 @@
+MPAreport_html <- function(peces, invertebrados, comunidad, reserva, control) {
+  
+  library(rmarkdown)
+  
+  title <- paste("Análisis para", reserva, "en la comunidad pesquera", comunidad, sep = " ")
+  
+  render(input = "ReporteSAM.Rmd",
+         params = list(title = title,
+                       peces = peces,
+                       invertebrados = invertebrados,
+                       comunidad = comunidad,
+                       reserva = reserva,
+                       control = control
+         ),
+         output_file = paste('/Reporte', comunidad, reserva, '.html', sep=''),
+         output_dir = getwd()
+  )
+}
+
+load("Datos/PecesSAM.RData")
+load("Datos/InvertebradosSAM.RData")
+
+Comunidad <- c("Maria Elena",
+              "Maria Elena",
+              "Maria Elena",
+              "Punta Herrero",
+              "Punta Herrero",
+              "Punta Herrero",
+              "Banco Chinchorro",
+              "Banco Chinchorro",
+              "Banco Chinchorro")
+Reserva <- c("Cabezo",
+            "El Gallinero",
+            "Punta Loria",
+            "Anegado de Chal",
+            "El Faro",
+            "Manchon",
+            "Zona Nucleo Cayo Norte",
+            "ZRP40 Cañones Norte",
+            "ZRP40 Cañones Sur")
+Control <- paste(Reserva, "(Control)", sep = " ")
+
+
+for (i in 4:length(Sitios$Comunidad)){
+  
+  comunidad <- Comunidad[i]
+  reserva <- Reserva[i]
+  control <- Control[i]
+  
+  MPAreport_html(peces, invertebrados, comunidad, reserva, control)
+  
+}
+
+
+
+
+
+
+
+
+
+
+
