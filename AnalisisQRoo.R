fb80902a94001acbe77257f04a22ea0b74bbf9c4
jcvdav <juanarlos.villader@gmail.com>
2016-08-26 18:00:43 -0700

    Actualicé los análisis de QRoo para incluir langosta y caraol en el resumen. Tambié arreglé los links en index.html

--- AnalisisQRoo.R
+++ AnalisisQRoo.R
@@ -1,21 +1,21 @@
-MPAreport_html <- function(peces, invertebrados, comunidad, reserva, control) {
-  
-  library(rmarkdown)
-  
-  title <- paste("Análisis para", reserva, "en la comunidad pesquera", comunidad, sep = " ")
-  
-  render(input = "ReporteSAM.Rmd",
-         params = list(title = title,
-                       peces = peces,
-                       invertebrados = invertebrados,
-                       comunidad = comunidad,
-                       reserva = reserva,
-                       control = control
-         ),
-         output_file = paste('/Reporte', comunidad, reserva, '.html', sep=''),
-         output_dir = getwd()
-  )
-}
+# MPAreport_html <- function(peces, invertebrados, comunidad, reserva, control) {
+#   
+#   library(rmarkdown)
+#   
+#   title <- paste("Análisis para", reserva, "en la comunidad pesquera", comunidad, sep = " ")
+#   
+#   render(input = "ReporteSAM.Rmd",
+#          params = list(title = title,
+#                        peces = peces,
+#                        invertebrados = invertebrados,
+#                        comunidad = comunidad,
+#                        reserva = reserva,
+#                        control = control
+#          ),
+#          output_file = paste('/Reporte', comunidad, reserva, '.html', sep=''),
+#          output_dir = getwd()
+#   )
+# }
 
 library(MPAtools)
 
@@ -27,14 +27,12 @@ Comunidad <- c("Maria Elena",
               "Maria Elena",
               "Punta Herrero",
               "Punta Herrero",
-              "Punta Herrero",
               "Banco Chinchorro",
               "Banco Chinchorro",
               "Banco Chinchorro")
 Reserva <- c("Cabezo",
             "El Gallinero",
             "Punta Loria",
-            "Anegado de Chal",
             "El Faro",
             "Manchon",
             "Zona Nucleo Cayo Norte",
@@ -49,32 +47,10 @@ for (i in 1:length(Comunidad)){
   reserva <- Reserva[i]
   control <- Control[i]
   
+  print(i)
+  
   MPAreport_html(peces, invertebrados, comunidad, reserva, control)
   
-}
-
-
-function (data, reserve = NULL, control = NULL, error.bars = F) 
-{
-  library(ggplot2)
-  library(dplyr)
-  library(tidyr)
-  if (is.null(reserve) | is.null(control)) {
-    stop("You must specify reserve and control sites")
-  }
-  colnames(data) <- c("Ano", "Zonificacion", "Sitio", "Transecto", 
-                      "Indicador")
-  data <- data %>% filter(Sitio == reserve | Sitio == control) %>% 
-    group_by(Ano, Sitio) %>% mutate(SD = sd(Indicador, na.rm = T), 
-                                    Indicator = mean(Indicador, na.rm = T))
-  p <- ggplot(data, aes(x = Ano, y = Indicator, color = Zonificacion, 
-                        pch = Sitio)) + geom_point() + geom_line() + theme_bw() + 
-    scale_color_brewer(palette = "Set1")
-  if (error.bars) {
-    p <- p + geom_errorbar(aes(ymin = Indicator - SD, ymax = Indicator + 
-                                 SD), width = 0.2)
-  }
-  p
 }
 
 
